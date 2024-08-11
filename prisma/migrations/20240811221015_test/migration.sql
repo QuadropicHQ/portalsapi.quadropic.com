711e6281fbe8840d5e08fc858373167ab6adc91c
Mohamed Kamran <mohamedkamran2211@icloud.com>
2024-08-12 04:14:42 +0530

    made readme.md

--- prisma/migrations/20240811221015_test/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "username" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("username")
-);
